44ddfb2d74885835b83ceba4241ded8866b88f51
Brian Reece <bdreece@mtu.edu>
2022-04-13 16:01:50 -0400

    Updated ReadBody function to return body length

--- util.go
+++ util.go
@@ -1,42 +1,37 @@
 package main
 
 import (
-	"encoding/json"
 	"io"
 	"net/http"
 
 	"github.com/bdreece/tattle"
 )
 
-func ReadBody(r *http.Request, l *tattle.Logger) []byte {
+func ReadBody(r *http.Request, l *tattle.Logger) ([]byte, int) {
 	// Read request body
-	var data []byte
+	var (
+		data []byte
+		total int
+	)	
 	buf := make([]byte, 1024)
 	for {
 		n, err := r.Body.Read(buf)
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
+
+		if err != nil && err != io.EOF {
 			l.Errf("Error reading request body: %s\n", err.Error())
 			continue
 		}
+		
 		if n > 0 {
-			data = append(data, buf...)
+			data = append(data, buf[:n]...)
+			total += n
+		}
+		
+		if err == io.EOF {
+			l.Logln("Reached EOF")
+			break
 		}
 	}
 
-	return data
-}
-
-func ReadJSON(r *http.Request, l *tattle.Logger) map[string]any {
-	body := make(map[string]any)
-	// Unmarshal data
-	data := ReadBody(r, l)
-	if err := json.Unmarshal(data, &body); err != nil {
-		l.Errf("Error unmarshaling request body: %s\n", err.Error())
-		return nil
-	}
-
-	return body
+	return data, total
 }
